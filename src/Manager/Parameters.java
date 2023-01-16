cdf2009a0dc7186836de2f53ffa04c5a8033ce8e
IbtesamOmeir <ibtesamomeir@gmail.com>
2023-01-16 14:54:54 +0000

    update

--- src/Manager/Parameters.java
+++ src/Manager/Parameters.java
@@ -9,20 +9,18 @@ import org.testng.annotations.BeforeTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Parameters {
-	WebDriver driver = new ChromeDriver() ; 
+	WebDriver driver = new ChromeDriver();
 
 	String HomePage = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login";
-	String TheManager = "Hermoine Granger" ;
+	String TheManager = "Hermoine Granger";
 	String Currency = "Dollar";
-	String SearchForCustomer="Hermoine";
-
+	String SearchForCustomer = "Hermoine";
 
 	@BeforeTest
 	public void setup() {
 		driver.get(HomePage);
 
-		WebDriverManager.chromedriver().setup();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3)); 
-
-}
+	 WebDriverManager.chromedriver().setup();
+	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+	}
 }
